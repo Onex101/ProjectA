e91df378693d4d3a76e79a0715cc042da11ae6ce
Antonio Kay <Antonio@marshan.co.za>
2019-03-13 09:52:07 +0200

    Task #001: HTTPS server trusted on host computer. Issue #001: HTTPS connection not trusted on LAN server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const app = require('express')();
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+    cert: fs.readFileSync('ProjectA.crt'),
+    key: fs.readFileSync('ProjectA.key')
+};
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+https.createServer(options, app).listen(port);
